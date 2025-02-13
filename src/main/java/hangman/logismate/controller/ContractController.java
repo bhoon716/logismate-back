11f28a00b81c0ecbf2e392b564aa350e47378705
bhoon <leafloveu@naver.com>
2025-02-13 09:13:06 +0900

    feat: ContractService 추가

---
+++ src/main/java/hangman/logismate/controller/ContractController.java
@@ -0,0 +1,13 @@
+package hangman.logismate.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/contract")
+@RequiredArgsConstructor
+public class ContractController {
+
+
+}
