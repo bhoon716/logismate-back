e4db0b4c27687aaa58536ec76456f565120045ab
bhoon <leafloveu@naver.com>
2025-02-13 21:59:49 +0900

    cors

--- src/main/java/hangman/logismate/config/CorsConfig.java
+++ src/main/java/hangman/logismate/config/CorsConfig.java
@@ -15,12 +15,11 @@ public class CorsConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**") // 모든 엔드포인트에 대해 CORS 허용
-                        .allowedOriginPatterns("*") // 모든 도메인에서 접근 허용
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS") // 모든 HTTP 메서드 허용
+                registry.addMapping("/**")
+                        .allowedOriginPatterns("*") // 모든 도메인 허용
+                        .allowedMethods("*") // 모든 HTTP 메서드 허용
                         .allowedHeaders("*") // 모든 요청 헤더 허용
-                        .allowCredentials(true) // 인증 정보 포함 허용
-                        .maxAge(3600); // 캐싱 시간 (초 단위)
+                        .allowCredentials(true); // 인증 정보 허용
             }
         };
     }
